9190462cdb7c8691dd2487c554debc13ccf9b92f
Joakim Fogelström <birdstream@hotmail.com>
2020-06-06 13:38:49 +0200

    fix

--- scripts/5.3_data_src_trained_mask_apply.sh
+++ scripts/5.3_data_src_trained_mask_apply.sh
@@ -3,5 +3,5 @@ source env.sh
 
 $DFL_PYTHON "$DFL_SRC/main.py" xseg apply \
     --input-dir "$DFL_WORKSPACE/data_src/aligned" \
-    --model.dir "$DFL_WORKSPACE/model"
+    --model-dir "$DFL_WORKSPACE/model"
 
