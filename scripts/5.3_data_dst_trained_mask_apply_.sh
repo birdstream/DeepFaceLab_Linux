5754041d36e0357178fdd3890d85eb2162a20230
Joakim Fogelström <birdstream@hotmail.com>
2020-06-06 13:36:25 +0200

    Oops again

--- scripts/5.3_data_dst_trained_mask_apply_.sh
+++ scripts/5.3_data_dst_trained_mask_apply_.sh
@@ -2,5 +2,5 @@
 source env.sh
 
 $DFL_PYTHON "$DFL_SRC/main.py" xseg apply \
-    --input-dir "$DFL_WORKSPACE/data_dst/aligned"
+    --input-dir "$DFL_WORKSPACE/data_dst/aligned" \
     --model-dir "$DFL_WORKSPACE/model"
